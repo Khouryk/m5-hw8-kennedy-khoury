0c33a10fe62ec914dd24fe592e1b53e48e1ccf37
Khoury Kennedy <khoury.kennedy@ufl.edu>
2022-03-15 17:21:06 -0400

    Add if else statement and display randomized lose msg

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 
 var lostMsg = [ 'Oh no!', ' Better luck next time!', 'Try again!'];
+var ranLoss= lostMsg[Math.floor(Math.random()*lostMsg.length)];
+
 
 // Create variables to count wins and losses
 
@@ -9,11 +11,11 @@ var winsCt= 0;
 var lossCt= 0;
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
-var msg = document.getElementById('message');
+var msg = document.getElementById('message').innerHTML;
 var wins = document.getElementById('wins');
 var lose = document.getElementById('losses');
 
-console.log(msg);
+
 // target all .box elements and attach a click event listener to each one using a loop
 
 let box1= parseInt(document.getElementById ("box-1").textContent);
@@ -30,42 +32,24 @@ console.log(box3);
 
 let boxs = document.querySelectorAll('.box');
 
-parseInt(boxs.forEach(function(i){
-  i.addEventListener( 'click', function(){
-  console.log(i);
+var boxClick= parseInt(boxs.forEach(function(i){
+  i.addEventListener('click', function(){
+  console.log(i.innerHTML);
  });
 
 })).textContent;
-
-console.log(boxs);
-
 
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
 
-
-
-
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
 
-function getRandomNumber(){
-   var randomNumber = Math.floor(Math.random()*3)+1;
-   console.log(randomNumber);
-   return randomNumber;
-}
-var ranNum = getRandomNumber();
+
 
 // TESTING HANDLECLICK to help target clicked boxes??//
-function handleClick() {
-   if (randomNumber === box1.valueOf){
-      console.log('congrats');
-   }
-   else {
-      console.log(lostMsg);
-   }
-}
+
 
 //const randomNumber = Math.floor(Math.random()*3)+1;
 //console.log(randomNumber);
@@ -83,5 +67,18 @@ function handleClick() {
 ;
 
 
+var randomNumber = Math.floor(Math.random()*3)+1;
+  console.log(randomNumber);
+
+//problem: runs as soon as page opens rather tahn after click, doesn't seem to be reading code, immediatly says failure msg
+if (boxClick ==randomNumber){
+   console.log('congrats');
+}
+else {
+    document.getElementById('message').innerHTML=ranLoss;
+}
+console.log(msg);
+
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+
